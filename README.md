60cda61bab1fb31fe30dae0fb256acf0f17dd6f4
lgwds <lgwiedemann@gmail.com>
2020-07-22 20:21:41 +0200

    added bash script to setup environment

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Hey! This is a clone of the tf-pose-estimation by Ildoo Kim modified to work with Tensorflow 2.0+!
 Link to original repo: https://www.github.com/ildoonet/tf-openpose
+
+
+Execute 'bash run.sh' to setup the conda env, install requirements and do a webcam test using openpose
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate condavenv 
+echo 'conda env activated'
+conda install python==3.7.6
+conda install tensorflow
+cd ./tf-pose-estimation
+pip install -r requirements.txt
+conda install swig
+cd ./tf_pose/pafprocess
+swig -python -c++ pafprocess.i && python3 setup.py build_ext --inplace
+pip install opencv-python
+pip install git+https://github.com/adrianc-a/tf-slim.git@remove_contrib
+cd ../..
+cd models/graph/cmu
+bash download.sh
+cd ../../..
+#python run.py --model=mobilenet_thin --resize=432x368 --image=./images/p1.jpg
+python run_webcam.py --model=mobilenet_thin --resize=432x368 --camera=0
